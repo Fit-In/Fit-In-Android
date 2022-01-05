23355836b4c62b67a8cb0468ad30141f3479b854
cheewr85 <cheewr85@naver.com>
2022-01-05 15:18:01 +0900

    HDA-7 코딩 컨벤션에 맞게 리팩토링 레이아웃 이슈 해결

--- Android/FitIn_v1/app/src/main/java/com/example/fitin_v1/CompleteActivity.java
+++ Android/FitIn_v1/app/src/main/java/com/example/fitin_v1/CompleteActivity.java
@@ -2,6 +2,10 @@ package com.example.fitin_v1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +17,16 @@ public class CompleteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_complete);
         setTitle("회원가입 완료");
         Intent inIntent  = getIntent();
+
+        TextView completeText = (TextView) findViewById(R.id.tv_completeText);
+        setPartColor(0,5,completeText);
+    }
+
+    private TextView setPartColor(int start, int end, TextView textLogin) {
+        String textData = textLogin.getText().toString();
+        SpannableStringBuilder spannable = new SpannableStringBuilder(textData);
+        spannable.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.morningGlory)),start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textLogin.setText(spannable);
+        return textLogin;
     }
 }
--- Android/FitIn_v1/app/src/main/java/com/example/fitin_v1/RegisterActivity.java
+++ Android/FitIn_v1/app/src/main/java/com/example/fitin_v1/RegisterActivity.java
@@ -1,10 +1,14 @@
 package com.example.fitin_v1;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,21 +21,33 @@ public class RegisterActivity extends AppCompatActivity {
         setTitle("회원가입");
 
         Intent inIntent = getIntent();
-        ImageButton ivBack = (ImageButton)findViewById(R.id.ivback);
-        ivBack.setOnClickListener(new View.OnClickListener() {
+        ImageButton buttonBack = (ImageButton)findViewById(R.id.btn_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
 
-        ImageButton btnRegister = (ImageButton)findViewById(R.id.BtnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        ImageButton buttonRegister = (ImageButton)findViewById(R.id.btn_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent outIntent = new Intent(getApplicationContext(), CompleteActivity.class);
                 startActivity(outIntent);
             }
         });
+
+        TextView findIdText = (TextView)findViewById(R.id.tv_findId);
+        setPartColor(11,14,findIdText);
+    }
+
+    private TextView setPartColor(int start, int end, TextView textLogin) {
+        String textData = textLogin.getText().toString();
+        SpannableStringBuilder spannable = new SpannableStringBuilder(textData);
+        spannable.setSpan(new StyleSpan(Typeface.BOLD),start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new StyleSpan(Typeface.BOLD),17,textLogin.getText().length(),Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textLogin.setText(spannable);
+        return textLogin;
     }
 }
